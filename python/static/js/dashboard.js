41bb91bcae6a5ae2d41ee63f49bcfb7da2cb9347
R0NAM1 <r0nam1@toasty.cafe>
2024-06-02 16:53:37 -0700

    Small random client changes.

--- python/static/js/dashboard.js
+++ python/static/js/dashboard.js
@@ -1,39 +1,7 @@
-function deleteCameraConfirm(cameraName) {
-  var modal = document.getElementById("passwordpopup");
-  // For User Reading
-  document.getElementById('popup-content').innerHTML = document.getElementById('popup-content').innerHTML.replace('[selectedCamera]', cameraName);
-  // For Form Submission
-  document.getElementById('form').innerHTML = document.getElementById('form').innerHTML.replace('[selectedCamera]', cameraName);
-  
-  modal.style.display = "block";
-  globalVars.currentCam = cameraName;
+function loadOther() {
+
+  // Grab select
+
 }
 
-var globalVars = {};
-
-window.onload = function () {
-
-  // Get the modal
-  var modal = document.getElementById("passwordpopup");
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName('close');
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  // Pass through variable camera name
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-        // For User Reading
-      document.getElementById('popup-content').innerHTML = document.getElementById('popup-content').innerHTML.replace(globalVars.currentCam, '[selectedCamera]');
-      // For Form Submission
-      document.getElementById('form').innerHTML = document.getElementById('form').innerHTML.replace(globalVars.currentCam, '[selectedCamera]');
-    }
-  } 
-
-};
+window.onload = loadOther;
